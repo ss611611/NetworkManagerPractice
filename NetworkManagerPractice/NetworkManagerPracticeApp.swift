495e2acf158133ba7f8e32c8e64a8ce58f4f44e7
jackie <ss611611@gmail.com>
2024-06-26 16:50:36 +0800

    Initial Commit

---
+++ NetworkManagerPractice/NetworkManagerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerPracticeApp.swift
+//  NetworkManagerPractice
+//
+//  Created by Jackie Lu on 2024/6/26.
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
